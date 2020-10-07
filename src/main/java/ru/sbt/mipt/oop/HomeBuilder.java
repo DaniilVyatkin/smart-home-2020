98656090f8761b153e2d622d7fa8239f2edce491
Daniil <vyatkin.da@phystech.edu>
2020-10-07 15:04:38 +0300

    Homework 1 on SmartHome

--- src/main/java/ru/sbt/mipt/oop/HomeBuilder.java
+++ src/main/java/ru/sbt/mipt/oop/HomeBuilder.java
@@ -1,14 +1,9 @@
 package ru.sbt.mipt.oop;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
+
+import static ru.sbt.mipt.oop.SmartHomeStateSaver.saveSmartHomeState;
 
 public class HomeBuilder {
 
@@ -26,13 +21,7 @@ public class HomeBuilder {
                 Arrays.asList(new Door(false, "4")),
                 "hall");
         SmartHome smartHome = new SmartHome(Arrays.asList(kitchen, bathroom, bedroom, hall));
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String jsonString = gson.toJson(smartHome);
-        System.out.println(jsonString);
-        Path path = Paths.get("output.js");
-        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-            writer.write(jsonString);
-        }
+        saveSmartHomeState(smartHome);
     }
 
 }
---
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeStateSaver.java
@@ -0,0 +1,24 @@
+package ru.sbt.mipt.oop;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SmartHomeStateSaver {
+    public static void saveSmartHomeState(SmartHome smartHome) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonString = gson.toJson(smartHome);
+        System.out.println(jsonString);
+        Path path = Paths.get("output.js");
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            writer.write(jsonString);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
